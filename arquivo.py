19b8d5cc72fddf4107cd78db795f7948a074fe67
Kélvisck Aureliano Cabral <202119060379@lab02-l>
2023-05-22 14:31:16 -0400

    feat:adicionando classe endereco

--- arquivo.py
+++ arquivo.py
@@ -6,6 +6,12 @@ class Pessoa:
         self.nome = nome
         self.idade = idade
 
+class Endereco:
+    def __init__(self, rua, numero):
+        self.rua = rua
+        self.idade = idade
+
+
 
 
 
